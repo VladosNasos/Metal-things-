09198d5cec681677c7bfc1a0531b8b1e8dcb8d17
kande <kande@LAPTOP-GQNI63IC>
2023-10-06 00:52:12 +0300

    Добавьте файлы проекта.

---
+++ ht_sharaga/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ht_sharaga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // distance betwen circles r/10
+
+            Console.Write("Lenght: ");
+            int lenght= int.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            int width = int.Parse(Console.ReadLine());
+            Console.Write("weight: ");
+            int weight=int.Parse(Console.ReadLine());
+            Console.Write("R: ");
+            int r= int.Parse(Console.ReadLine());
+
+            double remainingMetal = CalcScrap(lenght, width, weight, r);
+
+            Console.WriteLine("Answer is "+remainingMetal);
+
+        }
+        static double CalcScrap(double length1, double width1, double weight1, double r1)
+        {
+            // area of 1 circle
+            double circleArea = Math.PI * r1 * r1;
+
+            // calculating total amount of cirlces
+            double numCircles = (length1 - (r1 / 10)) / (2 * r1);
+
+            // calculating total area of circles 
+            double totalCircleArea = numCircles * circleArea;
+
+            // final calculations
+            double remainingMetal = weight1 - (totalCircleArea * width1);
+
+            return remainingMetal;
+        }
+    }
+}
